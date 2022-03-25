117efb68109b6689c8d1f308bd05c814e64b1238
youarhache <youarhache@gmail.com>
2022-03-25 16:14:19 +0100

    stric comparisoin tests

--- test_compare_tables.py
+++ test_compare_tables.py
@@ -1,6 +1,7 @@
-from compare_tables import compare_dataframes
 import pytest
+import numpy as np
 import pandas as pd
+from compare_tables import compare_dataframes
 
 
 @pytest.fixture()
@@ -16,8 +17,76 @@ def simple_dataframe():
     return df
 
 
-def test_compare_identical_tables(simple_dataframe):
+def test_compare_strict_identical_tables(simple_dataframe):
     second_df = simple_dataframe.copy()
+
     result = compare_dataframes(simple_dataframe, second_df)
 
-    assert result is None
+
+def test_compare_strict_identical_tables_with_nulls(simple_dataframe):
+    simple_dataframe.loc[2, "float_column"] = np.nan
+    second_df = simple_dataframe.copy()
+
+    result = compare_dataframes(simple_dataframe, second_df)
+
+
+def test_compare_strict_int_difference(simple_dataframe):
+    second_df = simple_dataframe.copy()
+    second_df.loc[2, "int_column"] = 7
+    expected_index = pd.MultiIndex.from_tuples(
+        [(2, "int_column")], names=["id", "column"]
+    )
+    expected = pd.DataFrame(
+        {
+            "from": [33],
+            "to": [7],
+        },
+        index=expected_index,
+        dtype=np.dtype("O"),
+    )
+
+    result = compare_dataframes(simple_dataframe, second_df)
+    assert isinstance(result, pd.DataFrame)
+    assert result.index == expected.index
+    assert (result.values == expected.values).all()
+    assert result.equals(expected)
+
+
+def test_compare_strict_str_difference(simple_dataframe):
+    second_df = simple_dataframe.copy()
+    second_df.loc[1, "str_column"] = "ZZ"
+    expected_index = pd.MultiIndex.from_tuples(
+        [(1, "str_column")], names=["id", "column"]
+    )
+    expected = pd.DataFrame(
+        {
+            "from": ["bb"],
+            "to": ["ZZ"],
+        },
+        index=expected_index,
+        dtype=np.dtype("O"),
+    )
+
+    result = compare_dataframes(simple_dataframe, second_df)
+    assert isinstance(result, pd.DataFrame)
+    assert result.equals(expected)
+
+
+def test_compare_strict_float_difference(simple_dataframe):
+    second_df = simple_dataframe.copy()
+    second_df.loc[0, "float_column"] = 3.14
+    expected_index = pd.MultiIndex.from_tuples(
+        [(0, "float_column")], names=["id", "column"]
+    )
+    expected = pd.DataFrame(
+        {
+            "from": [1.1],
+            "to": [3.14],
+        },
+        index=expected_index,
+        dtype=np.dtype("O"),
+    )
+
+    result = compare_dataframes(simple_dataframe, second_df)
+    assert isinstance(result, pd.DataFrame)
+    assert result.equals(expected)
