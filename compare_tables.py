aff9e812f1e7e82daffb59d221c418a3d97558aa
youarhache <youarhache@gmail.com>
2022-04-01 15:08:23 +0200

    comparing tables with tolerence

--- compare_tables.py
+++ compare_tables.py
@@ -1,10 +1,9 @@
 from typing import Optional, List
-from matplotlib.font_manager import findSystemFonts
 import pandas as pd
 import numpy as np
 
 
-def compare_dataframes(
+def compare_strictly(
     first_df: pd.DataFrame, second_df: pd.DataFrame
 ) -> Optional[pd.DataFrame]:
     if first_df.equals(second_df):
@@ -15,15 +14,33 @@ def compare_dataframes(
 
 
 def compare_with_tolerence(
-    first_df: pd.DataFrame, second_df: pd.DataFrame, tolerances: List[float]
+    first_df: pd.DataFrame,
+    second_df: pd.DataFrame,
+    absolute_tolerance: float = 0,
+    relative_tolerance: float = 0,
 ) -> Optional[pd.DataFrame]:
     if first_df.equals(second_df):
         return None
-    assert is_values_numeric(first_df) and is_values_numeric(second_df)
     diff_mask = pd.DataFrame()
-    for column in first_df.columns:
-        diff_mask[column] = first_df.loc[:, column].isclose(second_df.loc[:, column])
-    differences = format_differences(first_df, second_df, diff_mask)
+    non_numeric = first_df.select_dtypes([int, float])
+    non_numeric_columns = [c for c in first_df.columns if c not in non_numeric]
+    for column in non_numeric:
+        diff_mask[column] = ~np.isclose(
+            first_df.loc[:, column],
+            second_df.loc[:, column],
+            rtol=relative_tolerance,
+            atol=absolute_tolerance,
+            equal_nan=True,
+        )
+    diff_mask[non_numeric_columns] = (
+        first_df.loc[:, non_numeric_columns] != second_df.loc[:, non_numeric_columns]
+    ) & ~(
+        first_df.loc[:, non_numeric_columns].isnull()
+        & second_df.loc[:, non_numeric_columns].isnull()
+    )
+    differences = format_differences(
+        first_df, second_df, diff_mask.loc[:, first_df.columns]
+    )
     return differences
 
 
--- test_compare_tables.py
+++ test_compare_tables.py
@@ -1,21 +1,24 @@
-from unittest import result
-import pytest
 import numpy as np
 import pandas as pd
-from compare_tables import compare_dataframes
+import pytest
+from compare_tables import compare_strictly, compare_with_tolerence
 
 
 def test_compare_strict_identical_tables(simple_dataframe):
     second_df = simple_dataframe.copy()
 
-    result = compare_dataframes(simple_dataframe, second_df)
+    result = compare_strictly(simple_dataframe, second_df)
+
+    assert result is None
 
 
 def test_compare_strict_identical_tables_with_nulls(simple_dataframe):
     simple_dataframe.loc[2, "float_column"] = np.nan
     second_df = simple_dataframe.copy()
 
-    result = compare_dataframes(simple_dataframe, second_df)
+    result = compare_strictly(simple_dataframe, second_df)
+
+    assert result is None
 
 
 def test_compare_strict_int_difference(simple_dataframe):
@@ -33,7 +36,7 @@ def test_compare_strict_int_difference(simple_dataframe):
         dtype=np.dtype("O"),
     )
 
-    result = compare_dataframes(simple_dataframe, second_df)
+    result = compare_strictly(simple_dataframe, second_df)
     assert isinstance(result, pd.DataFrame)
     pd.testing.assert_frame_equal(result, expected)
 
@@ -53,7 +56,7 @@ def test_compare_strict_str_difference(simple_dataframe):
         dtype=np.dtype("O"),
     )
 
-    result = compare_dataframes(simple_dataframe, second_df)
+    result = compare_strictly(simple_dataframe, second_df)
     assert isinstance(result, pd.DataFrame)
     pd.testing.assert_frame_equal(result, expected)
 
@@ -73,6 +76,95 @@ def test_compare_strict_float_difference(simple_dataframe):
         dtype=np.dtype("O"),
     )
 
-    result = compare_dataframes(simple_dataframe, second_df)
+    result = compare_strictly(simple_dataframe, second_df)
+    assert isinstance(result, pd.DataFrame)
+    pd.testing.assert_frame_equal(result, expected)
+
+
+def test_compare_with_tolerance_identical_tables(simple_dataframe):
+    second_df = simple_dataframe.copy()
+
+    result = compare_with_tolerence(
+        simple_dataframe, second_df, absolute_tolerance=0.01
+    )
+
+    assert result is None
+
+
+def test_compare_with_tolerance_identical_tables_with_nulls(simple_dataframe):
+    simple_dataframe.loc[2, "float_column"] = np.nan
+    second_df = simple_dataframe.copy()
+
+    result = compare_with_tolerence(
+        simple_dataframe, second_df, absolute_tolerance=0.01, relative_tolerance=1e-6
+    )
+
+    assert result is None
+
+
+def test_compare_with_tolerance_int_difference(simple_dataframe):
+    second_df = simple_dataframe.copy()
+    second_df.loc[1, "int_column"] = 19
+    second_df.loc[2, "int_column"] = 35
+    expected_index = pd.MultiIndex.from_tuples(
+        [(1, "int_column")], names=["id", "column"]
+    )
+    expected = pd.DataFrame(
+        {
+            "from": [22],
+            "to": [19],
+        },
+        index=expected_index,
+        dtype=np.dtype("O"),
+    )
+
+    result = compare_with_tolerence(
+        simple_dataframe, second_df, absolute_tolerance=0, relative_tolerance=0.1
+    )
+    assert isinstance(result, pd.DataFrame)
+    pd.testing.assert_frame_equal(result, expected)
+
+
+def test_compare_with_tolerance_str_difference(simple_dataframe):
+    second_df = simple_dataframe.copy()
+    second_df.loc[1, "str_column"] = "ZZ"
+    expected_index = pd.MultiIndex.from_tuples(
+        [(1, "str_column")], names=["id", "column"]
+    )
+    expected = pd.DataFrame(
+        {
+            "from": ["bb"],
+            "to": ["ZZ"],
+        },
+        index=expected_index,
+        dtype=np.dtype("O"),
+    )
+
+    result = compare_with_tolerence(
+        simple_dataframe, second_df, absolute_tolerance=0.01, relative_tolerance=1e-6
+    )
+    assert isinstance(result, pd.DataFrame)
+    pd.testing.assert_frame_equal(result, expected)
+
+
+def test_compare_with_tolerance_float_difference(simple_dataframe):
+    second_df = simple_dataframe.copy()
+    second_df.loc[0, "float_column"] = 1.25
+    second_df.loc[1, "float_column"] = 2.11
+    expected_index = pd.MultiIndex.from_tuples(
+        [(0, "float_column")], names=["id", "column"]
+    )
+    expected = pd.DataFrame(
+        {
+            "from": [1.1],
+            "to": [1.25],
+        },
+        index=expected_index,
+        dtype=np.dtype("O"),
+    )
+
+    result = compare_with_tolerence(
+        simple_dataframe, second_df, absolute_tolerance=0.1, relative_tolerance=0
+    )
     assert isinstance(result, pd.DataFrame)
     pd.testing.assert_frame_equal(result, expected)
