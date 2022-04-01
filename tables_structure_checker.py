16fe95e809f0d508b6319844262207c885e2d2d5
youarhache <youarhache@gmail.com>
2022-04-01 16:47:23 +0200

    refactoring table structure checker

--- tables_structure_checker.py
+++ tables_structure_checker.py
@@ -1,18 +1,48 @@
 import re
+from xmlrpc.client import Boolean
 import pandas as pd
 
 
 class TablesStructureChecker:
     @staticmethod
-    def is_same_dtypes(first_df: pd.DataFrame, second_df: pd.DataFrame):
-        first_df = first_df.convert_dtypes()
-        second_df = second_df.convert_dtypes()
-        return first_df.dtypes.equals(second_df.dtypes)
+    def is_same_dtypes(first_table: pd.DataFrame, second_table: pd.DataFrame):
+        first_table = first_table.convert_dtypes()
+        second_table = second_table.convert_dtypes()
+        return first_table.dtypes.equals(second_table.dtypes)
 
     @staticmethod
-    def is_same_columns(first_df: pd.DataFrame, second_df: pd.DataFrame):
-        return first_df.columns.equals(second_df.columns)
+    def is_same_columns(first_table: pd.DataFrame, second_table: pd.DataFrame):
+        return first_table.columns.equals(second_table.columns)
 
     @staticmethod
-    def is_same_index(first_df: pd.DataFrame, second_df: pd.DataFrame):
-        return first_df.index.equals(second_df.index)
+    def is_same_index(first_table: pd.DataFrame, second_table: pd.DataFrame):
+        return first_table.index.equals(second_table.index)
+
+    @classmethod
+    def check_all(cls, first_table: pd.DataFrame, second_table: pd.DataFrame) -> bool:
+        if not cls.is_same_dtypes(first_table, second_table):
+            raise DifferentColumnTypesError(
+                f"The tables have different column types: \nfirst table:\n{first_table.dtypes.to_dict()}\n\nsecond table:\n{second_table.dtypes.to_dict()}"
+            )
+
+        if not cls.is_same_columns(first_table, second_table):
+            raise DifferentColumnsError(
+                f"The tables have different columns: \nfirst table:\n{first_table.columns.to_dict()}\n\nsecond table:\n{second_table.columns.to_dict()}"
+            )
+
+        if not cls.is_same_index(first_table, second_table):
+            raise DifferentIndexesError("The tables have different indexes")
+
+        return True
+
+
+class DifferentColumnTypesError(BaseException):
+    pass
+
+
+class DifferentColumnsError(BaseException):
+    pass
+
+
+class DifferentIndexesError(BaseException):
+    pass
